0a734cf0fc0e2f1d91a749d16c67f15551952462
berkaygurcan <berkaygurcan141@gmail.com>
2025-01-12 00:49:57 +0300

    This function has been removed from this page.

--- app/page.jsx
+++ app/page.jsx
@@ -2,10 +2,8 @@ import Hero from "@/components/Hero";
 import HomeProperties from "@/components/HomeProperties";
 import InfoBoxes from "@/components/InfoBoxes";
 import FeaturedProperties from "@/components/FeaturedProperties";
-import connectDB from "@/config/database";
 
 const HomePage =  () => {
-     connectDB()
     return ( <>
        <Hero />
        <InfoBoxes />
